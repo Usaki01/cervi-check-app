db38d5918e9af2a1e26cea71b545802431ee4cdc
Usaki01 <kratai37902@gmail.com>
2024-04-19 15:23:30 +0700

    init

--- Home.py
+++ Home.py
@@ -1,5 +1,5 @@
-from streamlit_extras.switch_page_button import switch_page
 import streamlit as st
+from streamlit_extras.switch_page_button import switch_page
 
 st.set_page_config(
     page_title="Cervical Cancer Risk Assessment",
